9c09ca91eb473a5115a94e6f143a17429b8290a6
phuong6124 <94773413+phuong6124@users.noreply.github.com>
2021-11-21 13:02:07 +0700

    Update nlar.js

--- javascript/nlar.js
+++ javascript/nlar.js
@@ -18,11 +18,11 @@ app.controller('myController', function($scope) {
 	$scope.modal4 = "Email";
 	$scope.modal5 = "Số điện thoại";
 	$scope.linkfb = "https://www.facebook.com/ttmp.6124";
-	$scope.linkzl = "m.me/ttmp.6124";
-	$scope.linksc = "m.me/ttmp.6124";
-	$scope.linkgm = "m.me/ttmp.6124";
-	$scope.phonenumber = "Đang cập nhật";
-	$scope.email = "maiphuong@gmail.com";
+	$scope.linkzl = "https://www.facebook.com/ttmp.6124";
+	$scope.linksc = "https://www.facebook.com/ttmp.6124";
+	$scope.linkgm = "https://www.facebook.com/ttmp.6124";
+	$scope.phonenumber = "0123456789";
+	$scope.email = "phuong@gmail.com";
 	$scope.title2 = "Câu chuyện của tôi";
 	$scope.contentStory1 = "Lmint là biệt danh đầu tiên và cũng là duy nhất của mình. Khi còn là cậu học sinh lớp 10 ngu ngơ, mình đã gặp anh Quang. Người đã thay đổi cuộc đời mình theo chiều hướng tích cực hơn. Biến mình từ thằng mọt sách không biết sử dụng máy tính trở thành một lập trình viên như hôm nay. Được tự hào kể lại câu chuyện và tự hào vì là một thành viên chân thành tại AnonyViet. Mình thực sự rất hạnh phúc khi học tập và làm việc tại đây.";
 	$scope.contentStory2 = "Trong suốt hai năm qua, tôi cảm thấy mình chưa có tiến bộ nhiều trong công việc lẫn học tập. Cảm thấy mình cần bổ sung nhiều kiến thức hơn, có trách nhiệm hơn trong công việc và lời hứa. Gần đây, website đã bước sang giai đoạn phát triển, member trông đợi khá nhiều từ tôi và các Admin khác. Cho nên tôi sẽ cố gắng nhiều hơn nữa để mọi người không thất vọng. Xin cảm ơn mọi người !";
